f23fb5dfac77ffbfe5859071193fe4d0b6cc35f5
andizabrina <159792981+andizabrina21@users.noreply.github.com>
2026-04-29 23:32:47 +0700

    Add files via upload

---
+++ scripts/bronze/ddl_dw_bronze_olist.sql
@@ -0,0 +1,109 @@
+--drop the table if it exist
+IF OBJECT_ID('bronze.crm_customers', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_customers;
+CREATE TABLE bronze.crm_customers(
+    customer_id NVARCHAR(50),
+    customer_unique_id NVARCHAR(50),
+    customer_zip_code_prefix INT,
+    customer_city NVARCHAR(50),
+    customer_state NVARCHAR(2)
+);
+
+
+IF OBJECT_ID('bronze.crm_order_reviews', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_order_reviews;
+CREATE TABLE bronze.crm_order_reviews(
+    review_id NVARCHAR(1000),
+    order_id NVARCHAR(1000),
+    review_score NVARCHAR(1000),
+    review_comment_title NVARCHAR(1000),
+    review_comment_message NVARCHAR(1000),
+    review_creation_date NVARCHAR(1000),
+    review_answer_timestamp NVARCHAR(1000)
+);
+
+
+IF OBJECT_ID('bronze.erp_order_items', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_order_items;
+CREATE TABLE bronze.erp_order_items(
+    order_id NVARCHAR(50),
+    order_item_id INT,
+    product_id NVARCHAR(50),
+    seller_id NVARCHAR(50),
+    shipping_limit_date DATETIME,
+    price FLOAT,
+    freight_value FLOAT
+);
+
+
+IF OBJECT_ID('bronze.erp_order_payments', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_order_payments;
+CREATE TABLE bronze.erp_order_payments(
+    order_id NVARCHAR(50),
+    payment_sequential INT,
+    payment_type NVARCHAR(50),
+    payment_installments INT,
+    payment_value FLOAT
+);
+
+
+IF OBJECT_ID('bronze.erp_orders', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_orders;
+
+CREATE TABLE bronze.erp_orders(
+    order_id NVARCHAR(50),
+    customer_id NVARCHAR(50),
+    order_status NVARCHAR(50),
+    order_purchase_timestamp DATETIME,
+    order_approved_at DATETIME,
+    order_delivered_carrier_date DATETIME,
+    order_delivered_customer_date DATETIME,
+    order_estimated_delivery_date DATETIME
+);
+
+
+IF OBJECT_ID('bronze.erp_products', 'U') IS NOT NULL
+	DROP TABLE bronze.erp_products;
+CREATE TABLE bronze.erp_products(
+    product_id NVARCHAR(50),
+    product_category_name NVARCHAR(50),
+    product_name_lenght INT,
+    product_description_lenght INT,
+    product_photos_qty INT,
+    product_weight_g INT,
+    product_length_cm INT,
+    product_height_cm INT,
+    product_width_cm INT
+);
+
+
+IF OBJECT_ID('bronze.lob_geolocation', 'U') IS NOT NULL
+	DROP TABLE bronze.lob_geolocation;
+CREATE TABLE bronze.lob_geolocation(
+    geolocation_zip_code_prefix INT,
+    geolocation_lat FLOAT,
+    geolocation_lng FLOAT,
+    geolocation_city NVARCHAR(50),
+    geolocation_state NVARCHAR(50)
+);
+
+
+IF OBJECT_ID('bronze.lob_sellers', 'U') IS NOT NULL
+	DROP TABLE bronze.lob_sellers;
+CREATE TABLE bronze.lob_sellers(
+    seller_id NVARCHAR(50),
+    seller_zip_code_prefix INT,
+    seller_city NVARCHAR(50),
+    seller_state NVARCHAR(2)
+);
+
+
+IF OBJECT_ID('bronze.lob_category_name_translation', 'U') IS NOT NULL
+	DROP TABLE bronze.lob_category_name_translation;
+
+CREATE TABLE bronze.lob_category_name_translation(
+    product_category_name NVARCHAR(50),
+    product_category_name_english NVARCHAR(50)
+);
+
+
